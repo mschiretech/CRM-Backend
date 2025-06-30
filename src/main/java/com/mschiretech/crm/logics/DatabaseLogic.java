7125cca99240afe954626402e0052af533f83836
srdashBABLU <srdashbablu143@gmail.com>
2025-06-30 19:08:17 +0530

    init

--- src/main/java/com/mschiretech/crm/logics/DatabaseLogic.java
+++ src/main/java/com/mschiretech/crm/logics/DatabaseLogic.java
@@ -1,7 +1,7 @@
 package com.mschiretech.crm.logics;
 
-public class DatabaseLogic {
-    public static void main(String[] args) {
-        System.out.println("hello !");
-    }
-}
+//public class DatabaseLogic {
+//    public static void main(String[] args) {
+//        System.out.println("hello !");
+//    }
+//}
